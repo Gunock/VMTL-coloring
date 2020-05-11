c2f1c8ade323025103efa7c9e4492c988e91d307
Konrad Marciniak <konrad.marciniak@itii.com.pl>
2020-05-11 17:59:38 +0200

    poprawa API

--- src/app/logic_api.py
+++ src/app/logic_api.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
@@ -22,45 +22,40 @@ class Graph:
     nodes = []
     edges = []
 
-    def addnode(self, n):
+    def add_node(self, n):
         self.nodes.append(n)
 
-    def addedge(self, e):
+    def add_edge(self, e):
         self.edges.append(e)
 
-    def saveasjson(self):
-        jsonGraph = '{'
-        jsonGraph += '"nodes": ['
+    def save_as_json(self):
+        dictionary = {
+            "nodes": [],
+            "edges": []
+        }
         for x in range(len(self.nodes)):
-            jsonGraph += '{'
-            jsonGraph += '"id": ' + '"n' + str(x) + '",'
-            jsonGraph += '"label": ' + '"Node ' + str(x) + '",'
-            jsonGraph += '"x": ' + self.nodes[x].x_pos + ","
-            jsonGraph += '"y": ' + self.nodes[x].y_pos + ","
-            jsonGraph += '"size": ' + str(2)
-            if(x == len(self.nodes)-1):
-                jsonGraph += '}'
-            else:
-                jsonGraph += '},'
-        jsonGraph += '],'
-        jsonGraph += '"edges": ['
+            new_node = {
+                "id": "n"+str(x),
+                "label": "Node"+str(x),
+                "x": int(self.nodes[x].x_pos),
+                "y": int(self.nodes[x].y_pos),
+                "size": 2
+            }
+            dictionary["nodes"].append(new_node)
         for x in range(len(self.edges)):
-            jsonGraph += '{'
-            jsonGraph += '"id": ' + '"e' + str(x) + '",'
-            jsonGraph += '"source": ' + '"n' + self.edges[x].id_1 + '",'
-            jsonGraph += '"target": ' + '"n' + self.edges[x].id_2 + '"'
-            if (x == len(self.edges) - 1):
-                jsonGraph += '}'
-            else:
-                jsonGraph += '},'
-        jsonGraph += ']'
-        jsonGraph += '}'
+            new_edge = {
+                "id": "e"+str(x),
+                "source": self.edges[x].id_1,
+                "target": self.edges[x].id_2
+            }
+            dictionary["edges"].append(new_edge)
         f = open('data/graph.json', "w")
-        f.write(jsonGraph)
+        f.write(json.dumps(dictionary))
         f.close()
 
 
 graph = Graph()
+
 
 def dir_last_updated(folder):
     return str(max(os.path.getmtime(os.path.join(root_path, f))
@@ -75,18 +70,18 @@ def index():
 
 
 @app.route("/addNode", methods=["POST"])
-def addN():
+def add_node():
     n = Node(request.form['x_pos'], request.form['y_pos'])
-    graph.addnode(n)
-    graph.saveasjson()
+    graph.add_node(n)
+    graph.save_as_json()
     return render_template("index.html", last_updated=dir_last_updated('data'))
 
 
 @app.route("/addEdge", methods=["POST"])
-def addE():
+def add_edge():
     e = Edge(request.form['id_1'], request.form['id_2'])
-    graph.addedge(e)
-    graph.saveasjson()
+    graph.add_edge(e)
+    graph.save_as_json()
     return render_template("index.html", last_updated=dir_last_updated('data'))
 
 
